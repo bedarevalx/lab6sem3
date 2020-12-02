3a596f193008e13b99cc36f21d29b62b59510990
Bedarev <bedarev.axl@mail.ru>
2020-12-02 22:30:11 +0700

    Добавьте файлы проекта.

---
+++ prog6.sem32/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace prog6.sem32
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
